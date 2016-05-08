889c28c3a3e0604f9d2769190a252846320bf811
A-nas <anass.laghouaouta@hotmail.fr>
2016-05-08 04:30:54 +0200

    refactoring HelloWorld template and add all Scenes files

--- Classes/SplashScene.cpp
+++ Classes/SplashScene.cpp
@@ -1,14 +1,14 @@
-#include "HelloWorldScene.h"
+#include "SplashScene.h"
 
 USING_NS_CC;
 
-Scene* HelloWorld::createScene()
+Scene* SplashScene::createScene()
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
     
     // 'layer' is an autorelease object
-    auto layer = HelloWorld::create();
+    auto layer = SplashScene::create();
 
     // add layer as a child to scene
     scene->addChild(layer);
@@ -18,7 +18,7 @@ Scene* HelloWorld::createScene()
 }
 
 // on "init" you need to initialize your instance
-bool HelloWorld::init()
+bool SplashScene::init()
 {
     //////////////////////////////
     // 1. super init first
@@ -29,7 +29,9 @@ bool HelloWorld::init()
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
+ 
 
+ /*
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
@@ -38,7 +40,7 @@ bool HelloWorld::init()
     auto closeItem = MenuItemImage::create(
                                            "CloseNormal.png",
                                            "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+                                           CC_CALLBACK_1(SplashScene::menuCloseCallback, this));
     
 	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
                                 origin.y + closeItem->getContentSize().height/2));
@@ -69,18 +71,18 @@ bool HelloWorld::init()
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
 
-    // add the sprite as a child to this layer
-    this->addChild(sprite, 0);
+    // add the as a child to this layer
+    this->addChild(sprite, 0); */
     
     return true;
 }
 
-
-void HelloWorld::menuCloseCallback(Ref* pSender)
+/*
+void SplashScene::menuCloseCallback(Ref* pSender)
 {
     Director::getInstance()->end();
 
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
-}
+} */

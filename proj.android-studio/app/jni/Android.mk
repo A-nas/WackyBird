d77dc4f5fd69412f0ff3e05cda693b97b8206570
A-nas <anass.laghouaouta@hotmail.fr>
2017-06-25 17:43:32 +0200

    cocos2d-x library deleted and config files are changed as well to speed up compiling time

--- proj.android-studio/app/jni/Android.mk
+++ proj.android-studio/app/jni/Android.mk
@@ -2,9 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d)
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/external)
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos)
+$(call import-add-path,$(LOCAL_PATH)/../../../../Developement/cocos2d-x-3.10/cocos/prebuilt-mk)
 
 LOCAL_MODULE := cocos2dcpp_shared
 
@@ -32,7 +30,7 @@ LOCAL_STATIC_LIBRARIES := cocos2dx_static
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,.)
+$(call import-module,./prebuilt-mk)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END
